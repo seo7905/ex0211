99642ede090d4050e9a38fc8309a1a079f7cc683
seo7905 <wjddks64@gmail.com>
2025-02-11 17:17:37 +0900

    insert eclipse code

---
+++ src/main/java/com/sist/intelli_ex0211/vo/Test4VO.java
@@ -0,0 +1,14 @@
+//package com.sist.intelli_ex0211.vo;
+//
+//public class Test4VO {
+//	private Test2VO test;
+//
+//	public Test2VO getTest() {
+//		return test;
+//	}
+//
+//	public void setTest(Test2VO test) {
+//		this.test = test;
+//	}
+//
+//}
